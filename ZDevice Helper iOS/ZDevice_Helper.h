c55e07fa1279667be0a2df2cb2d692274f13c24c
shekharVishwakarma <shekhar.vishwakarma@zensar.com>
2022-07-27 22:55:38 +0530

    Project added

---
+++ ZDevice Helper iOS/ZDevice_Helper.h
@@ -0,0 +1,18 @@
+//
+//  ZDevice_Helper.h
+//  ZDevice Helper
+//
+//  Created by SV59349 on 27/07/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZDevice_Helper.
+FOUNDATION_EXPORT double ZDevice_Helper_VersionNumber;
+
+//! Project version string for ZDevice_Helper.
+FOUNDATION_EXPORT const unsigned char ZDevice_Helper_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZDevice_Helper/PublicHeader.h>
+
+
